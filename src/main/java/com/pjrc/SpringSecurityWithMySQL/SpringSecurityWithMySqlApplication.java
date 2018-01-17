7acb160710904a226193edb65575ed5c7326bb09
PedroJesusRC <pedrojesus.rc@gmail.com>
2018-01-17 12:54:49 +0100

    Subiendo proyecto

---
+++ src/main/java/com/pjrc/SpringSecurityWithMySQL/SpringSecurityWithMySqlApplication.java
@@ -0,0 +1,12 @@
+package com.pjrc.SpringSecurityWithMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithMySqlApplication.class, args);
+	}
+}
